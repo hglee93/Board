27e6bb67d226e14ed188c019d24124dce5dd89d0
hglee93 <jeuslhg@gmail.com>
2020-06-12 20:29:36 +0900

    Add PostsService, PostsSaveRequestDto, PostsApiController 클래스 추가
    - 게시글 등록 기능 추가
    - Web Layer/Service Layer/Repository Layer의 각 역할
    - 도메인 모델

---
+++ src/main/java/com/hee/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.hee.book.springboot.service.posts;
+
+import com.hee.book.springboot.domain.posts.PostsRepository;
+import com.hee.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
