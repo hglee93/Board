4ef38352bfee90226f78f0e57d2a154b8af6aab0
hglee93 <jeuslhg@gmail.com>
2020-06-13 19:50:07 +0900

    Add 게시글 조회, 수정, 삭제 화면 만들기

--- src/main/java/com/hee/book/springboot/web/IndexController.java
+++ src/main/java/com/hee/book/springboot/web/IndexController.java
@@ -1,18 +1,39 @@
 package com.hee.book.springboot.web;
 
+import com.hee.book.springboot.service.posts.PostsService;
+import com.hee.book.springboot.web.dto.PostsListResponseDto;
+import com.hee.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<PostsListResponseDto> list = postsService.findAllDesc();
+        model.addAttribute("posts", list);
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
