cea4ae737fd5852265089d7c3ace415aa393d8fd
hglee93 <jeuslhg@gmail.com>
2020-06-15 20:23:15 +0900

    Update Login function.
    - 로그인 기능 업데이트
    - SessionUser를 따로 분리하여 사용하는 이유 : 객체직렬화
    - User Entity 클래스를 직렬화하여 사용하는 것은 위험하다.
      (Entity 클래스는 관계를 맺을 때 사용할 수 있기 때문에)

--- src/main/java/com/hee/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/hee/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -1,5 +1,7 @@
 package com.hee.book.springboot.config.auth;
 
+import com.hee.book.springboot.config.auth.dto.OAuthAttributes;
+import com.hee.book.springboot.config.auth.dto.SessionUser;
 import com.hee.book.springboot.domain.user.User;
 import com.hee.book.springboot.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,7 +26,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException{
-        /*OAuth2UserService delegate = new DefaultOAuth2UserService();
+        OAuth2UserService delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
@@ -37,7 +39,16 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         httpSession.setAttribute("user", new SessionUser(user));
 
         return new DefaultOAuth2User(
-                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey()), attributes.getAttributes(), attributes.getNameAttributeKey());
-        )*/
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey()))
+                , attributes.getAttributes()
+                , attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+
+        return userRepository.save(user);
     }
 }
