55c0d03fdbea77f0f4d95ddd3bb8ff4f96bc43cb
hglee93 <jeuslhg@gmail.com>
2020-06-15 23:53:38 +0900

    Add Login function.
    - 로그인 기능 완

--- src/main/java/com/hee/book/springboot/web/IndexController.java
+++ src/main/java/com/hee/book/springboot/web/IndexController.java
@@ -1,15 +1,16 @@
 package com.hee.book.springboot.web;
 
+import com.hee.book.springboot.config.auth.dto.SessionUser;
 import com.hee.book.springboot.service.posts.PostsService;
 import com.hee.book.springboot.web.dto.PostsListResponseDto;
 import com.hee.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -17,11 +18,17 @@ import java.util.List;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         List<PostsListResponseDto> list = postsService.findAllDesc();
         model.addAttribute("posts", list);
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
